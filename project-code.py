539c3175c47fa4dac7ba309d6b3eeca273771522
amirahadlina <107099161+amirahadlina@users.noreply.github.com>
2022-06-16 02:39:38 +0800

    Update project-code.py

--- project-code.py
+++ project-code.py
@@ -1,1 +1,50 @@
+import streamlit as st
+import pandas as pd
 
+from sklearn.ensemble import RandomForestClassifier
+
+st.write("""
+# Sloth Species Prediction
+This app predicts **Sloth Species** type!
+""")
+
+st.sidebar.header('User Input Parameters')
+
+def user_input_features():
+    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
+    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
+    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
+    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
+    data = {'sepal_length': sepal_length,
+            'sepal_width': sepal_width,
+            'petal_length': petal_length,
+            'petal_width': petal_width}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(df)
+
+iris = pd.read_csv("https://raw.githubusercontent.com/amirahadlina/Final-Assignment/main/IRIS.csv")
+X = iris.loc[:,['sepal_length','sepal_width','petal_length','petal_width']]
+Y = iris.loc[:,['species']]
+clf = RandomForestClassifier()
+clf.fit(X, Y)
+
+prediction = clf.predict(df)
+prediction_proba = clf.predict_proba(df)
+
+st.subheader('Class labels and their corresponding index number')
+st.table(["setosa", "versicolor", "virginica"])
+
+
+st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Iris_%28plant%29.jpg/640px-Iris_%28plant%29.jpg")
+  
+st.subheader('Prediction')
+#st.write(iris.target_names[prediction])
+st.write(prediction)
+
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
