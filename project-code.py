d725467cd35c5bd137bad444656eee4effdc7547
amirahadlina <107099161+amirahadlina@users.noreply.github.com>
2022-06-16 22:09:36 +0800

    Update project-code.py

--- project-code.py
+++ project-code.py
@@ -11,9 +11,9 @@ st.header("This app predicts **Sloth Species** type")
 st.sidebar.header('Please choose these parameters to predict your Sloth!')
 
 def user_input_features():
-    Claw_length = st.sidebar.slider('Claw Length(cm)', 1.50, 12.20, 7.31)
-    Sloth_body_size = st.sidebar.slider('Sloth Body Size(cm)', 45.0, 69.10, 61.8)
-    Tail_length = st.sidebar.slider('Tail Length(cm)', -2.50, 9.50, 4.5)
+    Claw_length = st.sidebar.slider('Claw Length', 1.50, 12.20, 7.31)
+    Sloth_body_size = st.sidebar.slider('Sloth Body Size', 45.0, 69.10, 61.8)
+    Tail_length = st.sidebar.slider('Tail Length', -2.50, 9.50, 4.5)
     data = {'claw_length': claw_length,
             'sloth_body_size': sloth_body_size,
             'tail_length': tail_length}
@@ -25,9 +25,9 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-iris = pd.read_csv("https://raw.githubusercontent.com/amirahadlina/Final-Project-Sloth-Species/main/sloth_data_cleaned.csv")
-X = iris.loc[:,['claw_length','sloth_body_size','tail_length']]
-Y = iris.loc[:,['species']]
+sloth = pd.read_csv("https://raw.githubusercontent.com/amirahadlina/Final-Project-Sloth-Species/main/sloth_data_cleaned.csv")
+X = sloth.loc[:,['claw_length','sloth_body_size','tail_length']]
+Y = sloth.loc[:,['species']]
 clf = RandomForestClassifier()
 clf.fit(X, Y)
 
@@ -41,7 +41,7 @@ st.table(["two_toed", "three_toed"])
 st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Iris_%28plant%29.jpg/640px-Iris_%28plant%29.jpg")
   
 st.subheader('Prediction')
-#st.write(iris.target_names[prediction])
+#st.write(sloth.target_specie[prediction])
 st.write(prediction)
 
 st.subheader('Prediction Probability')
