94538c8dcd7c9a278b986fbb6d16accbf0263413
amirahadlina <107099161+amirahadlina@users.noreply.github.com>
2022-06-16 23:59:10 +0800

    Update project-final.py

--- project-final.py
+++ project-final.py
@@ -18,7 +18,7 @@ def user_input_features():
     data = {'claw_length_cm': claw_length_cm,
             'size_cm': size_cm,
             'tail_length_cm': tail_length_cm}
-    features = pd.DataFrame(data, index[0])
+    features = pd.DataFrame(data)
     return features
 
 df = user_input_features()
