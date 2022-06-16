b9b3852b2cab86c444765e5b62e96cc7bbe1d436
amirahadlina <107099161+amirahadlina@users.noreply.github.com>
2022-06-16 22:29:39 +0800

    Update project-code.py

--- project-code.py
+++ project-code.py
@@ -14,7 +14,9 @@ def user_input_features():
     Claw_length = st.sidebar.slider('Claw Length', 1.50, 12.20, 7.31)
     Sloth_body_size = st.sidebar.slider('Sloth Body Size', 45.0, 69.10, 61.8)
     Tail_length = st.sidebar.slider('Tail Length', -2.50, 9.50, 4.5)
-    data = {'claw_length': claw_length,'sloth_body_size': sloth_body_size,'tail_length': tail_length}
+    data = {'claw_length': claw_length,
+            'sloth_body_size': sloth_body_size,
+            'tail_length': tail_length}
     features = pd.DataFrame(data, index=[0])
     return features
 
@@ -24,8 +26,7 @@ st.subheader('User Input parameters')
 st.write(df)
 
 sloth = pd.read_csv("https://raw.githubusercontent.com/amirahadlina/Final-Project-Sloth-Species/main/sloth_data_cleaned.csv")
-X = sloth.loc[:,['claw_length','sloth_body_size','tail_length']]
-Y = sloth.loc[:,['species']]
+
 clf = RandomForestClassifier()
 clf.fit(X, Y)
 
@@ -38,8 +39,8 @@ st.table(["two_toed", "three_toed"])
 
 st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Iris_%28plant%29.jpg/640px-Iris_%28plant%29.jpg")
   
-st.subheader('Prediction')
-#st.write(sloth.target_specie[prediction])
+#st.subheader('Prediction')
+st.write(sloth.target_specie[prediction])
 st.write(prediction)
 
 st.subheader('Prediction Probability')
