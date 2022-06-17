dfbd853165375bfac6a68c2ff558e4018a72f81d
amirahadlina <107099161+amirahadlina@users.noreply.github.com>
2022-06-17 13:33:23 +0800

    Update project-final.py

--- project-final.py
+++ project-final.py
@@ -22,7 +22,7 @@ def user_input_features():
     features = pd.DataFrame(data,index[0])
     return features
 
-df = features()
+df = features(pd.DataFrame)
 
 st.subheader('User Input parameters')
 st.write(df)
